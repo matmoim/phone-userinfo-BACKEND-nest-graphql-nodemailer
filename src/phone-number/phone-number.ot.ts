a064873531e990af65e634436e8f823cd49b5442
Matvii <roman@pop-os.localdomain>
2021-08-04 11:07:15 +0300

    allgood

--- src/phone-number/phone-number.ot.ts
+++ src/phone-number/phone-number.ot.ts
@@ -1,8 +1,8 @@
-import { Field, InputType,Entity } from "@nestjs/graphql";
-@Entity()
+import { Field, InputType, } from "@nestjs/graphql";
+
 @InputType()
-export class PhoneNumber{
-  @Field(() => Number, {nullable :true})
-    number:number;
-    }
+export class PhoneNumber {
+  @Field(() => Number, { nullable: true })
+  number: number;
+}
 
